435a645ef87f3e40c74138678fc1cf69a6f414c0
git-hub project <sushma97797@gmail.com>
2026-03-07 16:56:23 +0530

    update sendmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,8 +4,8 @@ export const sendEmail = async ({ to, subject, text }) => {
   //Transporter = postman , yeh decide karta hai konse email server or kaise authentication
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", //gmail ka smtp server
-    port: 465, //secure (ssl)
-    secure: true, //SSL encryption ON, Data safe rahta hai
+    port: 587, //secure (ssl)
+    secure: false, //SSL encryption ON, Data safe rahta hai
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, //yeh gmail password hai

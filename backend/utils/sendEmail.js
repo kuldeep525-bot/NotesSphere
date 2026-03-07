f26352ce3d6022ca9b4c716ab34eb884905a41db
git-hub project <sushma97797@gmail.com>
2026-03-07 18:47:37 +0530

    send email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -28,7 +28,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ to, subject, text }) => {
   await resend.emails.send({
-    from: process.env.EMAIL_FROM,
+    from: "NotesSphere <onboarding@resend.dev>",
     to,
     subject,
     text,
